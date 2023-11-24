74703fc5030310fdc66f0c2aed18922a4ecd62f1
lilshudd <max.fedorak10@gmail.com>
2023-11-24 10:02:21 +0200

    main

---
+++ js/01-gallery.js
@@ -0,0 +1,34 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryContainer = document.querySelector(".gallery");
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </div>
+      `;
+    })
+    .join("");
+}
+
+galleryContainer.innerHTML = createGalleryMarkup();
+
+const galleryImages = galleryContainer.querySelectorAll(".gallery__image");
+
+galleryImages.forEach((image, index) => {
+  image.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const lightbox = basicLightbox.create(`
+      <img src="${galleryItems[index].original}" alt="${galleryItems[index].description}" />
+    `);
+    lightbox.show();
+  });
+});
+
+console.log(galleryItems);
---
+++ js/02-lightbox.js
@@ -0,0 +1,23 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `;
+  })
+  .join("");
+
+galleryList.innerHTML = galleryMarkup;
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
